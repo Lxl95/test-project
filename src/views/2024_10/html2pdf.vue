ea78c1377e9b37fc3b8b9feb43ac462b2d6846da
lxl <1522689172@qq.com>
2024-11-06 11:27:28 +0800

    html包含svg, 导出PDF时未导出svg，注意html2canvas版本

--- src/views/2024_10/html2pdf.vue
+++ src/views/2024_10/html2pdf.vue
@@ -2,9 +2,16 @@
   <div>
     <el-button @click="generatePdf2">Generate PDF2</el-button>
 
-    <div id="pdfDom" ref="contentToConvert" style="position:fixed;z-index: -2000;top:2000px;width:100vw">
+    <div id="pdfDom" ref="contentToConvert" style="position: fixed; z-index: -2000; top: 2000px; width: 100vw">
       <!-- 这里放置你想要转换为PDF的内容 -->
       <h1>Hello, World!</h1>
+      <!-- svg导出失败，html2canvas版本@1.4.1 -->
+      <svg version="1.1" baseProfile="full" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="red" />
+        <circle cx="150" cy="100" r="80" fill="green" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+      </svg>
+
       <p>This is a sample content to convert into PDF.</p>
       <p>This is a sample content to convert into PDF.</p>
       <p>This is a sample content to convert into PDF.</p>
